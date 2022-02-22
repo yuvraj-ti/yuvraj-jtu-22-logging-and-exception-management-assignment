7d0544077c7d4a6954669f33333af38bf9d45ee0
nitish-ti <nitish.bharti@trilogy.com>
2022-02-22 17:29:35 +0530

    add xml parser and validation checker

---
+++ fast-api-als/utils/adf.py
@@ -0,0 +1,33 @@
+import xmltodict, json
+from jsonschema import Draft7Validator, validate, draft7_format_checker
+from utils.adf_schema import schema
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)0.8s] %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+def parse_xml(adf_xml):
+    try:
+        obj = xmltodict.parse(adf_xml)
+        return obj
+    except Exception as e:
+        logger.info(f"error in parsing input xml: {e}")
+        return None
+
+
+def check_validation(input_json):
+    try:
+        validate(
+            instance=input_json,
+            schema=schema,
+            format_checker=draft7_format_checker,
+        )
+        logger.info(f"input is validated")
+        return True, "input validated"
+    except Exception as e:
+        logger.error(f"Validation failed: {e.message}")
+        return False, e.message
