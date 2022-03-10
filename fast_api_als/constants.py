95688d5201ab9f0d5fe63be001b818937edbe365
nitish-ti <nitish.bharti@trilogy.com>
2022-03-10 12:25:15 +0530

    get DB TABLE NAME from environment

--- fast_api_als/constants.py
+++ fast_api_als/constants.py
@@ -6,4 +6,4 @@ ALS_AWS_ACCESS_KEY = os.getenv("ALS_AWS_ACCESS_KEY")
 ALS_AWS_REGION = os.getenv("ALS_AWS_REGION_NAME", "us-east-1")
 
 # DDB Constants
-DB_TABLE_NAME = 'auto-lead-scoring'
+DB_TABLE_NAME = os.getenv("ALS_DDB_TABLE_NAME", "auto-lead-scoring")
