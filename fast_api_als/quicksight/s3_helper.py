83017b3fc4e6726fa2c16027c858db62da94d92a
nitish-ti <nitish.bharti@trilogy.com>
2022-03-15 15:50:13 +0530

    add utility functions for creating quicksight data and s3 helper

---
+++ fast_api_als/quicksight/s3_helper.py
@@ -0,0 +1,32 @@
+import logging
+import json
+from fast_api_als import constants
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)0.8s] %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+class S3Helper:
+    def __init__(self, s3_client):
+        self.bucket_name = constants.S3_BUCKET_NAME
+        self.client = s3_client
+
+    def put_file(self, item, path):
+        res = self.client.put_object(
+            Body=json.dumps(item),
+            Bucket=self.bucket_name,
+            Key=path
+        )
+        verify_response(res, path)
+
+
+def verify_response(response, data):
+    status_code = response['ResponseMetadata']['HTTPStatusCode']
+    if not status_code == 200:
+        logger.error(f"Failed to add {data} to the database.")
+    else:
+        logger.info(f"New entry {data} added successfully.")
