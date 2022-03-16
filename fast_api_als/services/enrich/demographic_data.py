48bc98d5845755a72ac8001f9ec38e1799ce66e5
nitish-ti <nitish.bharti@trilogy.com>
2022-03-16 18:15:45 +0530

    get lat, lon from zipcode

--- fast_api_als/services/enrich/demographic_data.py
+++ fast_api_als/services/enrich/demographic_data.py
@@ -6,3 +6,8 @@ zipcode_search = SearchEngine()
 def find_demographic_data(zipcode):
     z = zipcode_search.by_zipcode(zipcode).to_dict()
     return z.get('median_household_income', 55319.39), z.get('population_density', 3585.81)
+
+
+def get_customer_coordinate(zipcode):
+    z = zipcode_search.by_zipcode(zipcode).to_dict()
+    return z.get('lat', 0), z.get('lng', 0)
