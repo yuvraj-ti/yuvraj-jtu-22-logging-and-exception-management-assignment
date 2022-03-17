00cfa7cc312f78e3a2600b740bc78d642df256cb
sahil-ti <sahil.bajaj@trilogy.com>
2022-03-17 15:03:43 +0530

    Add func to register new oem and 3pl

--- fast_api_als/utils/cognito_client.py
+++ fast_api_als/utils/cognito_client.py
@@ -1,5 +1,5 @@
 import boto3
-from fast_api_als.constants import ALS_AWS_ACCESS_KEY, ALS_AWS_REGION, ALS_AWS_SECRET_KEY
+from fast_api_als.constants import ALS_AWS_ACCESS_KEY, ALS_AWS_REGION, ALS_AWS_SECRET_KEY, ALS_USER_POOL_ID
 
 client = boto3.client(
     'cognito-idp',
@@ -14,4 +14,35 @@ def get_user_role(token: str):
     response = client.get_user(
         AccessToken=token
     )
-    return response['custom:name'], response['custom:role']
+    user_attribute = {}
+    for attr in response['UserAttributes']:
+        user_attribute[attr['Name']] = attr['Value']
+    return user_attribute.get('custom:name', ''), user_attribute.get('custom:role', '')
+
+
+def register_new_user(token: str, email: str, name: str, role: str):
+    response = client.get_user(
+        AccessToken=token
+    )
+    user_attribute = {}
+    for attr in response['UserAttributes']:
+        user_attribute[attr['Name']] = attr['Value']
+
+    if user_attribute.get('custom:role', '') != "ADMIN":
+        return "Unauthorized"
+
+    response = client.admin_create_user(
+        UserPoolId=ALS_USER_POOL_ID,
+        Username=email,
+        UserAttributes=[
+            {
+                'Name': 'custom:name',
+                'Value': name
+            },
+            {
+                'Name': 'custom:role',
+                'Value': role
+            },
+        ]
+    )
+    return response
