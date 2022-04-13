08e051c93ead767e169817d09c1c104b4d11774a
sahil-ti <sahil.bajaj@trilogy.com>
2022-04-13 17:47:28 +0530

    Add fetch all user by role and fetch user by custom:attribute for deletion

--- fast_api_als/utils/cognito_client.py
+++ fast_api_als/utils/cognito_client.py
@@ -1,9 +1,10 @@
 from fast_api_als.constants import ALS_USER_POOL_ID
 
 from fastapi import HTTPException
-from starlette.status import HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
 from fast_api_als.constants import ALS_AWS_ACCESS_KEY, ALS_AWS_REGION, ALS_AWS_SECRET_KEY, ALS_USER_POOL_ID
+from fast_api_als.routers.users import cognito_client
 
 
 def get_user_role(token: str, cognito_client):
@@ -94,17 +95,48 @@ def fetch_all_users_by_role(role, cognito_client):
     Returns:
         The users.
     """
+    if role not in ['ADMIN', 'OEM', '3PL']:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST, detail="Invalid role"
+        )
     res = cognito_client.list_users(
-        UserPoolId=ALS_USER_POOL_ID,
-        Filter="custom:role = {}".format(role)
+        UserPoolId=ALS_USER_POOL_ID
     )
     users = []
     for user in res['Users']:
         user_attribute = {}
+        role_match = False
+        for attr in user['Attributes']:
+            if attr['Name'] == 'custom:role' and attr['Value'] == role:
+                role_match = True
+            user_attribute[attr['Name']] = attr['Value']
+        if role_match:
+            users.append(user_attribute)
+    return users
+
+
+def find_sub_by_name(username, cognito_client):
+    """
+        Finds the sub by name.
+    Args:
+        username: The username of the user.
+        cognito_client: The cognito client to use.
+
+    Returns:
+        The sub.
+    """
+    res = cognito_client.list_users(
+        UserPoolId=ALS_USER_POOL_ID
+    )
+    for user in res['Users']:
+        user_attribute = {}
         for attr in user['Attributes']:
             user_attribute[attr['Name']] = attr['Value']
-        users.append(user_attribute)
-    return users
+        if user_attribute['custom:name'] == username:
+            return user_attribute['sub'], user_attribute['custom:role']
+    raise HTTPException(
+        status_code=HTTP_404_NOT_FOUND, detail="User not found"
+    )
 
 
 def congito_delete_user(username, cognito_client):
@@ -117,11 +149,12 @@ def congito_delete_user(username, cognito_client):
     Returns:
         The success or reject response.
     """
+    sub, role = find_sub_by_name(username, cognito_client)
     res = cognito_client.admin_delete_user(
         UserPoolId=ALS_USER_POOL_ID,
-        Username=username
+        Username=sub
     )
     if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return "SUCCESS"
+        return "SUCCESS", role
     else:
-        return "REJECT"
+        return "REJECT", role
