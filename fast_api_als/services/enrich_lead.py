58d0b0cd0b0ba4537e91c19abe662b9f90c25c97
yuvraj-ti <yuvraj.sharma@trilogy.com>
2022-06-29 07:52:31 +0000

    Added loggings and Exception hanlers in services/enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -13,3 +13,16 @@ what exceptions can be thrown here?
 
 def get_enriched_lead_json(adf_json: dict) -> dict:
     pass
+    if not isinstance(adf_json, dict):
+        logging.error('services -> get_enriched_lead_jso : adj_json does not matches the required dict datatype')
+        raise TypeError
+
+    x={
+        'key':'value'
+    }
+    if not isinstance(x,dict):
+        logging.error('services -> get_enriched_lead_jso : x does not matches the required returned dict datatype')
+        raise TypeError
+
+    return x
+
